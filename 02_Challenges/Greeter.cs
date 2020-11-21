ab9c7d7c45ca381d407370409a3dfaf5b7c93f07
Enrique Lopez <enmlopez@gmail.com>
2020-11-21 15:49:38 -0500

    Add project files.

---
+++ 02_Challenges/Greeter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Challenges
+{
+    public class Greeter
+    {
+        public string Greet(string name)
+        {
+            return "Hello " + name;
+        }
+
+        public string Bye(string name)
+        {
+            return "Later " + name;
+        }
+
+        //access modifer --return type -- [name(parameters) or method signature]
+        public void GiveMeTimeOfDay(DateTime greeting)
+        {
+            int hour = greeting.Hour;
+            if (hour >= 5 & hour < 12)
+            {
+                Console.WriteLine("Good morning!");
+            }
+            else if (hour >= 12 && hour < 16)
+            {
+                Console.WriteLine("Good afternoon!");
+            }
+            else if (hour >= 16 && hour < 20)
+            {
+                Console.WriteLine("Good evening!");
+            }
+            else
+            {
+                Console.WriteLine("Good night!");
+
+            }
+
+        }
+
+        public void GivemeTimeofDay(int hour)
+        {
+           
+            if (hour >= 5 & hour < 12)
+            {
+                Console.WriteLine("Good morning!");
+            }
+            else if (hour >= 12 && hour < 16)
+            {
+                Console.WriteLine("Good afternoon!");
+            }
+            else if (hour >= 16 && hour < 20)
+            {
+                Console.WriteLine("Good evening!");
+            }
+            else
+            {
+                Console.WriteLine("Good night!");
+
+            }
+        }
+    }
+}
---
+++ 02_Challenges/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greeter greeter = new Greeter();
+            string person = greeter.Greet("Joshua\n");
+            Console.WriteLine(person);
+
+            string person2 = greeter.Bye("Johua\n");
+            Console.WriteLine(person2);
+
+            DateTime greeting = DateTime.Now;
+            greeter.GiveMeTimeOfDay(greeting);
+
+            DateTime newGreeting = new DateTime(2020, 10, 31, 5, 20, 3);
+            greeter.GiveMeTimeOfDay(newGreeting);
+
+            greeter.GivemeTimeofDay(17);
+
+            Console.WriteLine("**************");
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
+
